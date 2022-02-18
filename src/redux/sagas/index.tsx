b23c211db2e2be3d0728b3d5fbb1aeed94728353
Corete95 <fanthell95@gmail.com>
2022-02-18 17:17:08 +0900

    [Modify]프록시 삭제

--- src/redux/sagas/index.tsx
+++ src/redux/sagas/index.tsx
@@ -7,7 +7,7 @@ import userSaga from "./userSaga";
 // import dotenv from "dotenv";
 // dotenv.config();
 
-// axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
+axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 export default function* rootSaga() {
   yield all([fork(authSaga), fork(postSaga), fork(userSaga)]);
